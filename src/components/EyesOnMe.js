b6bcc3df4640155bef89e95a38ca8b1600978651
shady-mungai <shadrackmungai10@gmail.com>
2023-01-13 14:06:38 +0300

    Completed Assingment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function handleFocus(event){
+    return console.log("Good!")
+}
+function handleBlur(){
+    return console.log("Hey! Eyes on me!")
+}
+function EyesOnMe() {
+  return (
+    <>
+
+      <button onBlur={handleBlur} onFocus={handleFocus}>Eyes on me</button>
+    </>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
 
+function handleChange(event){
+    return (
+        console.log("Entering password...")
+        )
+}
+
+
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input onChange={handleChange}type="password" />
+        </div>
     )
 }
 
